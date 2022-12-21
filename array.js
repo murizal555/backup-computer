a674926bbb8919b5c14a1dc4adc862256f036036
ijal <murizal444.com>
2022-12-21 21:12:17 +0800

    update

---
+++ array.js
@@ -0,0 +1,18 @@
+const fruits= ['apple','kopi','a'];
+document.write(fruits.length);
+
+
+// const fruits2= new Array ("paku","kayu","bow");
+// console.log(fruits2.length);
+
+// const fruits3=("jagung,tomat").join(',')
+// console.log(fruits3.length);
+
+
+
+
+//untuk mengambil sebelah
+const fruitz= ['aple','dod','aa'];
+const jagung=['h','i','m'];
+const fruitzString= fruitz.slice(jagung);
+console.log(fruitzString);
